e47749ea6268a9a43dac7d71cc9607f864e2f817
Break <chenyifei@blackjack-inc.com>
2021-12-28 11:34:11 +0800

    添加 fmt 库

--- Engine/Source/Runtime/Core/Private/Math/Transform.cpp
+++ Engine/Source/Runtime/Core/Private/Math/Transform.cpp
@@ -1,8 +1,9 @@
+#include <fmt/format.h>
+
 #include "Math/Transform.h"
 
 std::string Transform::ToString()
 {
-    std::string Result = "Translation";
-
+    std::string Result = fmt::format("Translation = [{}, {}, {}]", this->Translation.X, this->Translation.Y, this->Translation.Z);
     return Result;
 }
