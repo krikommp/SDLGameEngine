484f517436c1387532ad959221abdc2768c05a09
kriko <1136223096@qq.com>
2022-01-16 13:48:38 +0800

    笔记

--- TestGame/Src/MyAppEngine.cpp
+++ TestGame/Src/MyAppEngine.cpp
@@ -19,16 +19,16 @@ void MyAppEngine::OnStart() {
 
     srand(0);
 }
-
+constexpr int Num = 1000;
 void MyAppEngine::OnUpdate() {
     std::vector<FVector2i> Points;
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < Num; i++)
     {
         FVector2i P(rand() % 1280, rand() % 720);
         Points.push_back(P);
     }
     int i = 0;
-    while (i < 100)
+    while (i < Num)
     {
         DrawLine(RHI, Points[i], Points[i + 1], FColor(rand() % 255, rand() % 255, rand() % 255, 255));
         i += 2;
